a4c9bb5dc7a08329667251b2bed3e255d7803209
Darren Kim <terdong@gmail.com>
2023-02-03 01:22:45 +0900

    Fix classic_fm_hls_down.sh

--- linux/classic_fm_hls_down.sh
+++ linux/classic_fm_hls_down.sh
@@ -1,40 +1,57 @@
 #!/bin/bash
 
-#url=`curl -s 'https://cfpwwwapi.kbs.co.kr/api/v1/landing/live/channel_code/60_24' | jq -r '.channel_item[].service_url'`
-
-#echo $url
+function _error(){
+	echo "$1"
+	exit 1
+}
 
 today_date=$(date "+%y%m%d_%H")
-#tmp="echo hey! %s"
-#tmp2=$(printf "$tmp" "hi")
-#$tmp2
-if [ -n "$1" ];then
-	arg=$1
-	destination=/archives/contents_00/music/radio/classic_fm
-    url=`curl -s 'https://cfpwwwapi.kbs.co.kr/api/v1/landing/live/channel_code/60_24' | jq -r '.channel_item[].service_url'`
-	if [ $arg -eq 11 ];then
+hour=$(date "+%H")
+url=`curl -s 'https://cfpwwwapi.kbs.co.kr/api/v1/landing/live/channel_code/60_24' | jq -r '.channel_item[].service_url'`
+
+if [[ ! "$url" =~ ".m3u8" ]];then
+	_error "Failed to fetch a url."	
+fi
+
+if [ -n "$1" -a -d "$1" ];then
+	destination=$1
+	if [ $hour -eq 11 ];then
 		title="팝브런치"
 		duration=01:00:00
-	elif [ $arg -eq 12 ];then
+	elif [ $hour -eq 12 ];then
 		title="생생클래식"
 		duration=02:00:00
-	elif [ $arg -eq 17 ];then
+	elif [ $hour -eq 17 ];then
 		title="FM풍류마을"
 		duration=01:00:00
-	elif [ $arg -eq 22 ];then
+	elif [ $hour -eq 22 ];then
 		title="당신의밤과음악"
 		duration=02:00:00
-	elif [ $arg -eq 24 ];then
+	elif [ $hour -eq 24 ];then
 		title="JAZZ수첩"
 		duration=01:00:00
-	else
+	elif [ $hour -eq 18 ];then
 		title="세상의모든음악"
 		duration=02:00:00
 	fi
-	file_name="${today_date}_${title}"
-	#echo $file_name
 
-	#duration=`expr $duration \* 5`
-    sudo ffmpeg -i $url -c copy -t $duration "${destination}/${file_name}.mp4"
+	if [ -n "$title" ];then
+		file_name="${today_date}_${title}.mp4"
+		tmp_path="/tmp/${file_name}"
+		ffmpeg -i $url -c copy -t $duration $tmp_path && mv -f $tmp_path $destination
+	else
+		_error "Now($hour) is not the right time to record."
+	fi
+else
+	_error "no directory: $1"
 fi
+
+### test codes ###
+#url=`curl -s 'https://cfpwwwapi.kbs.co.kr/api/v1/landing/live/channel_code/60_24' | jq -r '.channel_item[].service_url'`
+#ffmpeg -f concat -safe 0 -i <(for f in ./*.aac; do echo "file '$PWD/$f'"; done) -c copy playlist.mp4
+#tmp="echo hey! %s"
+#tmp2=$(printf "$tmp" "hi")
+#$tmp2
+#echo $file_name
+#duration=`expr $duration \* 5`
 
